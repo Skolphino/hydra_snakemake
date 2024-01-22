ade9ac50c53f5606b373059a4803386fb27f677e
Jona Ackerschott <jona.a99@gmail.com>
2024-01-22 09:36:14 +0100

    simplify and make safer

--- wrapper.py
+++ wrapper.py
@@ -1,22 +1,23 @@
 from snakemake.shell import shell
 
-params = snakemake.params
-if not hasattr(params, 'script'):
-    raise Exception('Every rule using this wrapper needs to define script under params.')
-script = params['script']
-command = f'python {script} '
+if len(snakemake.params.keys()) != 1 or not 'script' in snakemake.params.keys():
+    raise Exception('Every rule using this wrapper needs'
+            'to define script as one and the only keyword param.')
+
+# script is the only keyword param, hence it will always be last
+script = snakemake.params.pop(-1)
+
 # # Would ideally do this but snakemake can't handle nested definitions in parameters
 # # ie) can't define datamodule.batch_size = 128
 # for key, value in snakemake.params.items(): 
 #     if key != 'script': # Can't delete this from the dict for some reason
 #         command += f'{key}={value} '
-for cmnd in snakemake.params[:-1]:
-    for opt in ['input.', 'output.', 'params.', 'wildcards.']:
-        if opt in cmnd:
-            cmnd = cmnd.replace(opt, f'snakemake.{opt}')
-    cmnd = eval(f'f"""{cmnd}"""')
-    command += f'{cmnd} '
-shell(command)
-# # Not sure how else to debug except to print the command
-# print(command)
-# shell(f'touch {snakemake.output}')
+params_context = snakemake.__dict__.copy()
+args = []
+for option in snakemake.params:
+    option = option.replace("{hs:", "{")
+    exec(f"arg = f'{option}'", {}, params_context)
+    args.append(params_context["arg"])
+args = " ".join(args)
+
+shell(f"python {script} {args}")
